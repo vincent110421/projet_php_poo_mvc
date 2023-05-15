4d1191b2a01755a3d6c7a278be5e400a82fcca8d
VincentV <v@v.v>
2023-05-15 13:55:38 +0200

    Ajout code 404 sur la page 404

--- src/Controllers/MainController.php
+++ src/Controllers/MainController.php
@@ -23,7 +23,11 @@ class MainController{
 
     public function page404(): void
     {
-    require VIEWS_DIR . '/404.php';
+            // Modifie le code HTTP pour qu'il soit bien 404 et non 200
+            header('HTTP/1.1 404 Not Found');
+
+             // Charge la vue "404.php" dans le dossier "views"
+             require VIEWS_DIR . '/404.php';
     }
 
 }
